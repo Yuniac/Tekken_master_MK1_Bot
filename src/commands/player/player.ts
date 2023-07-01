37e1d0f07f40ff7d739d40d7074136aaf21f55c0
Yuniac <yusuffathisabbag@gmail.com>
2023-07-01 14:50:35 +0300

    Added player info command

---
+++ src/commands/player/player.ts
@@ -0,0 +1,43 @@
+import {
+  CacheType,
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+} from "discord.js";
+import UserModal from "../../models/user";
+import { format } from "date-fns";
+
+const data = new SlashCommandBuilder()
+  .setName("player")
+  .setDescription("Gets info about a specific player")
+  .addUserOption((option) =>
+    option
+      .setName("name")
+      .setDescription("The name of the player to get info about")
+      .setRequired(true)
+  );
+
+const execute = async (interaction: ChatInputCommandInteraction<CacheType>) => {
+  const user = interaction.options.getUser("name");
+  const mognoUser = await UserModal.findOne({
+    name: user?.username,
+  });
+
+  if (!mognoUser || !user) {
+    return interaction.reply(
+      "Looks like this user isn't registered yet. We have no info about them."
+    );
+  }
+
+  const { id, discordId, name, points, rank, createdAt } = mognoUser;
+
+  interaction.reply(`
+      Here's the info that we know about **${name}**:
+  
+      -ID: ${discordId}
+      -Points: ${points}
+      -Rank: ${rank}
+      -Registered at: ${format(new Date(createdAt), "dd-MM-Y K:a")}
+    `);
+};
+
+export { data, execute };
