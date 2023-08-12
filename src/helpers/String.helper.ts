d141c96712e5baee19e386c8ddc45fa3c0b3f66f
Yuniac <yusuffathisabbag@gmail.com>
2023-08-12 15:07:55 +0300

    Updated scoreboard message

--- src/helpers/String.helper.ts
+++ src/helpers/String.helper.ts
@@ -183,7 +183,7 @@ Once there is data available, we will make sure to display it here!
     });
 
     const message = `
-The scoreboard (Powerd by Tekken Master Leaderboard bot. Refreshes every 3 hours. Last refresh was ${date})
+The scoreboard (Powerd by Tekken Master Leaderboard bot. Refreshes every 3 hours. The scoreboard was last refreshed at ${date})
 
 ${data}    
 `;
